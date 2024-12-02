4826a52a21dd5e7e04cf92902ba46fb17504fc7a
Haruki <127585577+Aaaakkkkbot@users.noreply.github.com>
2024-12-02 19:41:37 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -2,17 +2,17 @@ from os import path, getenv
 import os, time 
 
 class Config:
-    API_ID = int(getenv("API_ID", ""))
-    API_HASH = getenv("API_HASH", "")
-    BOT_TOKEN = getenv("BOT_TOKEN", "")
+    API_ID = int(getenv("API_ID", "21418386"))
+    API_HASH = getenv("API_HASH", "aeac46b1d123e82fe6dcb43b6a26cfae")
+    BOT_TOKEN = getenv("BOT_TOKEN", "7746109405:AAEt5sIJPunBLSw7Cq4CpwMtHLooJqrF2Ks")
  
-    ADMIN = list(map(int, getenv("ADMIN", "").split()))
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
-    FORCE_SUB = int(getenv("FORCE_SUB", ""))
+    ADMIN = list(map(int, getenv("ADMIN", "5496176944").split()))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002440247365"))
+    FORCE_SUB = int(getenv("FORCE_SUB", "Aero_Bots_tm"))
 
     # database configs
-    DB_URL = os.environ.get("DB_URL", "")
-    DB_NAME = os.environ.get("DB_NAME", "")
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://TaktAsahina99:TaktAsahina99@cluster0.iq3cx2j.mongodb.net/?retryWrites=true&w=majority")
+    DB_NAME = os.environ.get("DB_NAME", "Shanbot")
     
     #web response 
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
